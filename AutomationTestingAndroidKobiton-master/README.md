d8a4942edcf6a9b3523e00ec26de30714244553e
Harpreet Singh <hari.harpreetsingh25@gmail.com>
2020-05-04 13:29:30 +0530

    Android Automation

---
+++ AutomationTestingAndroidKobiton-master/README.md
@@ -0,0 +1,13 @@
+# AutomationTestingAndroid
+
+npm init
+
+npm install wd
+
+npm install mocha
+
+npm install babel-register
+
+
+Note:- On SignUpPage.js you need to change the phone number everytime if you want to signup the new user  eg:- SignUpPage.js file
+2) On Login.js file you need to enter the registered phone number to login the user
---
+++ AutomationTestingAndroidKobiton-master/TestAutomation/pages/helpers/desiredCapabilities.js
@@ -0,0 +1,22 @@
+exports.options ={
+
+    desiredCapabilities : {
+
+        sessionName:        'Savvy Build 1.0',
+        sessionDescription: '',
+        deviceOrientation:  'portrait',
+        captureScreenshots: true,
+        app:                'kobiton-store:54505',
+        groupId:            992, // Group: SavvyQA
+        deviceGroup:        'KOBITON',
+        deviceName:         'Galaxy A9 (2018)',
+        platformVersion:    '8.0.0',
+        platformName:       'Android'
+
+    },
+    Server : {
+    protocol: 'https',
+    host: 'api.kobiton.com',
+    auth: 'HarpreetSavvy:c0f93aeb-f5a4-4649-838d-f88a54bcbb7b'
+}
+};
